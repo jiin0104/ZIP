51b2218636cc4163f6f978dd1eac16863ded38b3
KitaBlack <jhk709401@gmail.com>
2023-06-21 15:43:10 +0900

    2023.06.21 김민철

---
+++ html/layouts/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="zip-footer-area">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 d-md-flex align-items-center justify-content-between">
+          <div class="footer-text">
+            <p>
+              Copyright &copy;<script>
+                document.write(new Date().getFullYear());
+              </script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
+                href="https://github.com/Dogonem/ZIP" target="_blank">전과자들</a>
+            </p>
+          </div>
+          <div class="footer-social-btns">
+            <a href="#"><i class="fa fa-linkedin" aria-haspopup="true"></i></a>
+            <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-haspopup="true"></i></a>
+            <a href="#"><i class="fa fa-facebook" aria-haspopup="true"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
