44adeb12ea611dcef92b3aa142f208ac4030c721
KitaBlack <jhk709401@gmail.com>
2023-07-03 14:34:24 +0900

    Header, Footer-vue

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,7 +1,17 @@
-<template>
-    <footer class="mt-5 py-5 bg-dark text-white">
-      &copy; 2023
-    </footer>
+  <template>
+    <footer class="dorne-footer-area">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 d-md-flex align-items-center justify-content-between">
+                  <div class="footer-text">
+                      <p>
+Copyright &copy;2023 All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://github.com/Dogonem/ZIP" target="_blank">전과자들</a>
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
   </template>
   <script>
   export default {
