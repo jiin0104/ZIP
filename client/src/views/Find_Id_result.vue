fcf999b3e8f9546c997c9a8041ff2ba14a963960
hongpac <cjsdn6466@naver.com>
2023-07-07 14:15:26 +0900

    vue,css 파일추가

---
+++ client/src/views/Find_Id_result.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <h4>아이디 찾기</h4>
+    </div>
+    <div class="description">
+      <p>고객님의 정보와 일치하는 아이디 목록입니다.</p>
+    </div>
+    <div class="form-container">
+      <form>
+        <div class="found-ids">
+          <div class="box">
+            <ul class="ids-list">
+              <li>
+                <strong>
+                  <label>아이디 : </label>
+                  <span>(찾은 아이디 표시, 로직구현 필요)</span>
+                </strong>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="options">
+          <li class="option-item">
+            <RouterLink to="/Login">로그인</RouterLink>
+          </li>
+          <li class="option-item">
+            <RouterLink to="/find_idpw">비밀번호 찾기</RouterLink>
+          </li>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+@import 'Find_Id_Result.css';
+</style>
+
+<script>
+export default {
+  name: "find_id_result",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ client/src/views/Find_Pw_Result.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>비밀번호 찾기</h2>
+      <div class="underline"></div>
+    </div>
+    <div class="description">
+      <p>고객님 아이디의 비밀번호입니다.</p>
+    </div>
+    <div class="form-container">
+      <form>
+        <div class="found-password">
+          <div class="box">
+            <ul class="password-list">
+              <li>
+                <strong>
+                  <label>(찾은 비밀번호 표시)</label>
+                </strong>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="btn-area">
+          <li class="btn-item">
+            <RouterLink to="/Login">로그인</RouterLink>
+          </li>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+@import 'Find_Pw_Result.css';
+</style>
+
+<script>
+export default {
+  name: "find_pw_result",
+  data() {
+    return {};
+  },
+};
+</script>
