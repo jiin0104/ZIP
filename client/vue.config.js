532ecf3dc5dab9dddfb88226333e798d2b98d78a
KitaBlack <jhk709401@gmail.com>
2023-07-10 11:14:13 +0900

    path vue -> node배포

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
-const target = 'http://127.0.0.1:3000';
+var path = require("path");
 
 module.exports = {
+  outputDir: path.resolve("../server/public"),
   devServer: {
-    port: 8080,
     proxy: {
       '^/api': {
-        target,
+        target: 'http://localhost:3000/api',
         changeOrigin: true
       },
       //필요없는 설정. 후에 우리가 업로드,다운로드 폴더를 쓸 경우 살림.
