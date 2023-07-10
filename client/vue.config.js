dec0c8f18a851218c611e7b6294b49446547b317
KitaBlack <jhk709401@gmail.com>
2023-07-10 11:18:22 +0900

    수정

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3000/api',
+        target: "http://localhost:3000",
         changeOrigin: true
       },
       //필요없는 설정. 후에 우리가 업로드,다운로드 폴더를 쓸 경우 살림.
