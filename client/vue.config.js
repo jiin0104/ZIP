aa986f6c781cabab3f7ac3bc406ff6db1ec3d357
Dogonem <aquakyesang1@gmail.com>
2023-07-11 10:38:49 +0900

    개개인별 sql문 확인 필요

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,18 @@
 var path = require("path");
 
+const target = 'http://127.0.0.1:3000';
+
 module.exports = {
   outputDir: path.resolve("../server/public"),
   devServer: {
+    port: 8080,
     proxy: {
-      "^/api": {
-        target: "http://localhost:3000",
+      '^/api': {
+        target,
         changeOrigin: true,
       },
       "/oath2.0": {
-        target: "https://nid.naver.com",
+        target,
       },
       //필요없는 설정. 후에 우리가 업로드,다운로드 폴더를 쓸 경우 살림.
       // '^/upload': {
