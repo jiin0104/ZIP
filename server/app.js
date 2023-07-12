03ca9561721be44c529fb2fc16efed77f7e679e3
Dogonem <aquakyesang1@gmail.com>
2023-07-12 13:49:27 +0900

    app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const passport = require("passport"); //로그인 로직할 때 필요
 
 const app = express();
 
-//쿠키 설정. 쿠키사용 보류
+// 쿠키 설정. 쿠키사용 보류
 // app.use(
 //   session({
 //     secret: "secret code",
@@ -27,12 +27,11 @@ const app = express();
 //passport 모듈 연결
 //passportConfig();
 //req객체에 passport설정 대입하는 미들웨어
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
 //req.session객체에 passport 정보 저장 미들웨어
 //*req.session 객체는 express-session에서 생성하므로,passport미들웨어는 express-session 미들웨어보다 뒤에 연걸 해야함.순서 중요.
-
-//app.use(passport.session());
+// app.use(passport.session());
 
 //express 서버로 POST 요청을 할 때 input 태그의 value를 전달하기 위해 사용
 //post 방식으로 클라이언트가 요청하는 본문에 있는 value를 넘겨받고 req.body 객체로 만들어주는 미들웨어.
@@ -65,18 +64,85 @@ const db = {
   host: "127.0.0.1",
   port: 3306,
   user: "root",
-  password: "12345678",
+  password: "root",
   connectionLimit: 100,
   multipleStatements: true, // 세미콜론으로 이어진 여러 개의 쿼리문을 한꺼번에 날릴 수 있게
 };
 
-//createconnection 말고 createpool을 이용해서 연결.createConnection은 단일 연결 방식, 요청이 있을 때마다 연결 객체를 생성했다가, 제거하는 것이 반복.
+//createconnection 말고 createpool을 이용해서 연결.
+//createConnection은 단일 연결 방식, 요청이 있을 때마다 연결 객체를 생성했다가, 제거하는 것이 반복.
 //따라서 비용, 시간, 연결에 대한 부담이 발생
 const dbPool = require("mysql2").createPool(db);
 
-app.post("/api/login", async (request, res) => {
-  // request.session['email'] = 'seungwon.go@gmail.com';
-  // res.send('ok');
+//이미지 업로드 불러오기 정의(나중에 슬라이드사진으로 할경우에는 수정필요)
+app.post("/upload/:ACCO_ID/:type/:fileName", async (request, res) => {
+  let { ACCO_ID, fileName } = request.params;
+  const dir = `${__dirname}/uploads/${ACCO_ID}`;
+  const file = `${dir}/${fileName}`;
+  if (!request.body.data)
+    return fs.unlink(file, async (err) =>
+      res.send({
+        err,
+      })
+    );
+  const data = request.body.data.slice(
+    request.body.data.indexOf(";base64,") + 8
+  );
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  fs.writeFile(file, data, "base64", async (error) => {
+    await req.db("accommodations", [
+      {
+        ACCO_ID: ACCO_ID,
+        //type: type,
+        path: fileName,
+      },
+    ]);
+
+    if (error) {
+      res.send({
+        error,
+      });
+    } else {
+      res.send("ok");
+    }
+  });
+});
+//이미지 불러오기
+app.get("/download/:ACCO_ID/:fileName", (request, res) => {
+  const { ACCO_ID, fileName } = request.params;
+  const filepath = `${__dirname}/uploads/${ACCO_ID}/${fileName}`;
+  res.header(
+    "Content-Type",
+    `image/${fileName.substring(fileName.lastIndexOf("."))}`
+  );
+  if (!fs.existsSync(filepath))
+    res.send(404, {
+      error: "Can not found file.",
+    });
+  else fs.createReadStream(filepath).pipe(res);
+});
+
+//apirole은 로그인된거 나중에 상품등록할때 관리자 권한으로 사용할것
+app.post("/apirole/:alias", async (request, res) => {
+  if (!request.session.email) {
+    return res.status(401).send({
+      error: "You need to login.",
+    });
+  }
+
+  try {
+    res.send(await req.db(request.params.alias));
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
+});
+
+// route 설정 목록
+
+//로그인 라우터. 웹페이지'/login'에서 인증로직 처리.
+app.post("/login", async (request, res) => {
   try {
     await req.db("signUp", request.body.param);
     if (request.body.param.length > 0) {
@@ -95,75 +161,11 @@ app.post("/api/login", async (request, res) => {
   }
 });
 
+//로그아웃 라우터. 로그아웃 시 세션 삭제
 app.post("/api/logout", async (request, res) => {
   request.session.destroy();
   res.send("ok");
 });
-
-app.post("/upload/:productId/:type/:fileName", async (request, res) => {
-  let { productId, type, fileName } = request.params;
-  const dir = `${__dirname}/uploads/${productId}`;
-  const file = `${dir}/${fileName}`;
-  if (!request.body.data)
-    return fs.unlink(file, async (err) =>
-      res.send({
-        err,
-      })
-    );
-  const data = request.body.data.slice(
-    request.body.data.indexOf(";base64,") + 8
-  );
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-  fs.writeFile(file, data, "base64", async (error) => {
-    await req.db("productImageInsert", [
-      {
-        product_id: productId,
-        type: type,
-        path: fileName,
-      },
-    ]);
-
-    if (error) {
-      res.send({
-        error,
-      });
-    } else {
-      res.send("ok");
-    }
-  });
-});
-
-app.get("/download/:productId/:fileName", (request, res) => {
-  const { productId, type, fileName } = request.params;
-  const filepath = `${__dirname}/uploads/${productId}/${fileName}`;
-  res.header(
-    "Content-Type",
-    `image/${fileName.substring(fileName.lastIndexOf("."))}`
-  );
-  if (!fs.existsSync(filepath))
-    res.send(404, {
-      error: "Can not found file.",
-    });
-  else fs.createReadStream(filepath).pipe(res);
-});
-
-app.post("/apirole/:alias", async (request, res) => {
-  if (!request.session.email) {
-    return res.status(401).send({
-      error: "You need to login.",
-    });
-  }
-
-  try {
-    res.send(await req.db(request.params.alias));
-  } catch (err) {
-    res.status(500).send({
-      error: err,
-    });
-  }
-});
-
-// route 설정 목록
 
 // 네이버 라우트. 검토 필요
 // router.get("/login/naver", passport.authenticate("naver"));
@@ -194,29 +196,6 @@ let corsOption = {
 };
 
 app.use(cors(corsOption)); //CORS 미들웨어
-
-// 지인테스트 - 지워도 무방
-app.get("/userlist", (req, res) => {
-  var sql = "SELECT * FROM users where user_no=1";
-  dbPool.query(sql, (error, rows) => {
-    if (error) throw error;
-    console.log("User info is : ", rows);
-    res.send(rows);
-  });
-});
-
-// app.get("/reslist", (req, res) => {
-//   var sql = "SELECT  FROM project.users";
-//   dbPool.query(sql, (error, rows) => {
-//     if (error) throw error;
-//     console.log("Res info is : ", rows);
-//     res.send(rows);
-//   });
-// });
-// app.all("/*", function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-// });
 
 // 쿼리 요청을 보내는 부분. 에러가 발생하였을 때 콘솔에 출력해주는 소스.
 app.post("/api/:alias", async (request, res) => {
