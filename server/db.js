7e3e3a5cfa5932751f09f0e25c62e607d01c7cee
Dogonem <aquakyesang1@gmail.com>
2023-07-13 14:51:39 +0900

    원상태로 돌림

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = {
   database: "project",
   host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: "root",
-});
+  multipleStatements: true,
+};
 
 db.connect((err) => {
   if (err) {
