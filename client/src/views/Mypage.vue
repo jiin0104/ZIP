1c4e8330c08baf979d0542864f8d917f40881c78
jiin0104 <jiin21c@naver.com>
2023-07-18 17:13:39 +0900

    회원정보수정

--- client/src/views/Mypage.vue
+++ client/src/views/Mypage.vue
@@ -1,72 +1,47 @@
 <template>
   <v-main>
     <!-- ***** Contact Area Start ***** -->
-    <div
-      class="dorne-contact-area d-md-flex container"
-      style="padding-top: 43px"
-      id="contact"
-    >
+    <div class="dorne-contact-area d-md-flex container" style="padding-top: 43px" id="contact">
       <!-- 버튼부분-->
       <div style="width: 20%">
         <div class="leftsidebar" style="width: 20%; padding-top: 30%">
-          <button
-            type="button"
-            id="mypage_button"
-            style="max-width: 20px; color: red"
-            @click="Mypage_Link"
-          >
+          <button type="button" id="mypage_button" style="max-width: 20px; color: red" @click="Mypage_Link">
             내정보
           </button>
-          <button
-            type="button"
-            id="mypage_button"
-            style="max-width: 20px"
-            @click="My_reservation_Link"
-          >
+          <button type="button" id="mypage_button" style="max-width: 20px" @click="My_reservation_Link">
             예약내역
           </button>
-          <button
-            type="button"
-            id="mypage_button"
-            style="max-width: 20px"
-            @click="My_Delete_Link"
-          >
+          <button type="button" id="mypage_button" style="max-width: 20px" @click="My_Delete_Link">
             회원 탈퇴
           </button>
         </div>
       </div>
       <!-- 여기가 오른쪽에 있는 내용 -->
-      <div
-        class="contact-form-area equal-height container"
-        style="padding-top: 6%"
-      >
+      <div class="contact-form-area equal-height container" style="padding-top: 6%">
         <div class="contact-text">
           <h1>내 정보</h1>
           <div class="user-info">
             <!-- 폼전송시 전달되는 data target element -->
-            <input aria-hidden="true" type="hidden" name="uno" />
-            <p>
-              <strong>아이디:{{ userlist.user_id }}</strong>
-            </p>
-            <p>
-              <strong>닉네임:{{ userlist.user_nickname }}</strong>
-            </p>
-            <p>
-              <strong>전화번호:{{ userlist.user_tel }}</strong>
-            </p>
-            <p>
-              <strong
-                >주소:{{ userlist.user_address1
-                }}{{ userlist.user_address2 }}</strong
-              >
-            </p>
-            <br /><button
-              id="infobutton"
-              @click="mypage_pw_check_Link"
-              style="display: inline-block"
-            >
+            <div class="border border-5" style="padding: 30px; display: inline-block; position: relative; right: 75px;">
+              <input aria-hidden="true" type="hidden" name="uno" />
+              <p>
+                <strong>아이디:{{ userlist.user_id }}</strong>
+              </p>
+              <p>
+                <strong>닉네임:{{ userlist.user_nickname }}</strong>
+              </p>
+              <p>
+                <strong>전화번호:{{ userlist.user_tel }}</strong>
+              </p>
+              <p>
+                <strong>주소:{{ userlist.user_address1
+                }}&nbsp;&nbsp;{{ userlist.user_address2 }}</strong>
+              </p>
+            </div>
+            <br /><br><button id="infobutton" @click="mypage_pw_check_Link" style="display: inline-block">
               정보수정
             </button>
+
           </div>
         </div>
       </div>
@@ -82,19 +57,10 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 export default {
   data() {
     return {
-      // user 정보를 담을 리스트
-      // USER_ID: "",
-      // USER_NICKNAME: "",
-      // USER_TEL: "",
-      // USER_ADDRESS1: "",
-      // USER_ADDRESS2: "",
+
       userlist: {},
     };
   },
-  // mounted() {
-  //   //페이지가 실행되자마자 작동시킬함수 정의
-  //   this.Get_User_Info();
-  // },
 
   created() {
     this.Get_User_Info();
@@ -102,7 +68,6 @@ export default {
 
   methods: {
     async Get_User_Info() {
-      // AXIOS로 통신하는 변수
       let userlist = await this.$api("/api/userlist", {});
       this.userlist = userlist[0];
     },
