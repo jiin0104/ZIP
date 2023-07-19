a6a25a030251109413442c7aef511f61c53f7399
jiin0104 <jiin21c@naver.com>
2023-07-19 15:10:18 +0900

    마이페이지 css수정, 호텔이미지 추가

--- client/src/views/Mypage.vue
+++ client/src/views/Mypage.vue
@@ -22,20 +22,40 @@
           <h1>내 정보</h1>
           <div class="user-info">
             <!-- 폼전송시 전달되는 data target element -->
-            <div class="border border-5" style="padding: 30px; display: inline-block; position: relative; right: 75px;">
+            <div style="padding: 30px; display: inline-block; position: relative; right: 75px;">
               <input aria-hidden="true" type="hidden" name="uno" />
               <p>
-                <strong>아이디:{{ userlist.USER_ID }}</strong>
+                <strong><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  </svg>&nbsp;&nbsp;아이디:</strong>&nbsp;{{ userlist.USER_ID }}
               </p>
               <p>
-                <strong>닉네임:{{ userlist.USER_NICKNAME }}</strong>
+                <strong><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-chat-square-heart" viewBox="0 0 16 16">
+                    <path
+                      d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12ZM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Z" />
+                    <path d="M8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
+                  </svg>&nbsp;&nbsp;닉네임:</strong>&nbsp;{{ userlist.USER_NICKNAME }}
               </p>
               <p>
-                <strong>전화번호:{{ userlist.USER_TEL }}</strong>
+                <strong><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-telephone" viewBox="0 0 16 16">
+                    <path
+                      d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                  </svg>&nbsp;&nbsp;전화번호:</strong>&nbsp;{{ userlist.USER_TEL }}
               </p>
               <p>
-                <strong>주소:{{ userlist.USER_ADDRESS1
-                }}&nbsp;&nbsp;{{ userlist.USER_ADDRESS2 }}</strong>
+                <strong><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-sign-turn-left" viewBox="0 0 16 16">
+                    <path
+                      d="M11 8.5A2.5 2.5 0 0 0 8.5 6H7V4.534a.25.25 0 0 0-.41-.192L4.23 6.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 7 8.466V7h1.5A1.5 1.5 0 0 1 10 8.5V11h1V8.5Z" />
+                    <path fill-rule="evenodd"
+                      d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435Zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134Z" />
+                  </svg>&nbsp;&nbsp;주소:</strong>&nbsp;{{ userlist.USER_ADDRESS1
+                  }}&nbsp;&nbsp;{{ userlist.USER_ADDRESS2 }}
               </p>
             </div>
             <br /><br><button id="infobutton" @click="mypage_pw_check_Link" style="display: inline-block">
@@ -68,7 +88,7 @@ export default {
 
   methods: {
     async Get_User_Info() {
-      let userlist = await this.$api("/api/userlist", {param:[this.$store.state.userId]});
+      let userlist = await this.$api("/api/userlist", { param: [this.$store.state.userId] });
       this.userlist = userlist[0];
     },
 
